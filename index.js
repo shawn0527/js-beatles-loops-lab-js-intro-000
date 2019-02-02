5afb60fdbd4cbd4cb77feeb64ffad8bff9fdab5f
Shawn Ren <doggy-ren@hotmail.com>
2019-02-02 02:07:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+var newArray=[]
+  for (var i=0; i<musicians.length; i++) {
+    newArray.push(musicians[i]+' plays '+instruments[i])
+  }
+  return newArray
+}
+
+function johnLennonFacts(array) {
+  var newArray=[]
+  var i=0
+  while(i<array.length) {
+    newArray.push(array[i]+'!!!')
+  }
+  i++
+  return newArray
+}
